0c4247dc93abefc8ae081acb900f68641dab0f1c
Focilia <melvinsalonga2999@gmail.com>
2022-10-23 14:50:52 +0800

    removed google maps api

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -134,7 +134,7 @@
 
     </div>
   </div>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1933.8689731629743!2d121.16559305790793!3d14.210116497653816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd63d4928bb879%3A0xce3e9ca02e88aa5d!2s459%20Villla%20Silangan%2C%20Calamba%2C%204027%20Laguna!5e0!3m2!1sen!2sph!4v1643646068942!5m2!1sen!2sph" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+  <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1933.8689731629743!2d121.16559305790793!3d14.210116497653816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd63d4928bb879%3A0xce3e9ca02e88aa5d!2s459%20Villla%20Silangan%2C%20Calamba%2C%204027%20Laguna!5e0!3m2!1sen!2sph!4v1643646068942!5m2!1sen!2sph" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> -->
 </section>
 <section id="footer">
   <div class="container pt-5">
